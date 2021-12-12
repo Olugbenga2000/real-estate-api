0f4ffbb99a49dd86cefe3e57e7c38e9838ccff8d
Olugbenga2000 <41525551+Olugbenga2000@users.noreply.github.com>
2021-12-12 17:02:47 +0100

    Update settings.py

--- Real_estate/settings.py
+++ Real_estate/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w$i)nx2658(*l*o6pqtc2jhmw)3wasz1&^dnd*3s8p0r8@2*%#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
